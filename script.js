f930dd0e696279cc63c0f76822e349f8bd4a1b39
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2025-02-10 10:18:56 +0530

    deleted:    images/blog2.jpg 	new file:   images/login.jpg 	modified:   index.html 	new file:   login.html 	new file:   register.html 	modified:   script.js 	new file:   styles.css

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active')
     searchBar.classList.toggle('active')
     cartItem.classList.remove('active')
+    window.location.hash = "#products";
 }
 
 document.querySelector('#cart-btn').onclick = () =>{
@@ -22,6 +23,36 @@ document.querySelector('#cart-btn').onclick = () =>{
 
 window.onscroll=() =>{
     navbar.classList.remove('active')
-    searchBar.classList.remove('active')
     cartItem.classList.remove('active')
 }
+
+// document.querySelector(".search-form").addEventListener("click", function () {
+//     window.location.hash = "#products";
+// }
+// )
+
+document.getElementById("search-box").addEventListener("input", function () {
+
+    let searchValue = this.value.toLowerCase();
+    let productItems = document.querySelectorAll(".products .box");
+    let menuItems = document.querySelectorAll(".menu .box");
+
+    function filterItems(items) {
+        items.forEach(item => {
+            let itemName = item.querySelector("h3").textContent.toLowerCase();
+            if (itemName.includes(searchValue)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    }
+
+    filterItems(productItems);
+    filterItems(menuItems);
+
+    
+    // if (searchValue.length > 0) {
+    //     window.location.hash = "#products";
+    // }
+});
